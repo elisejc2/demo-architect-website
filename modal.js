1400c4f9d224fd05ccfd4d2b0f4c07342f47fab1
Elise <128067735+elisejc2@users.noreply.github.com>
2024-10-08 13:40:23 -0400

    renamed low res imgs; changed a few of the imgs in modal.js

--- modal.js
+++ modal.js
@@ -3,36 +3,56 @@ const modal = document.getElementById("myModal");
 
 //const projects = document.querySelectorAll('.modal-trigger');
 
-const carouselImages = document.getElementsByClassName('modal-carousel-image');
+const carouselImages = document.getElementsByClassName('modal-carousel-image');//class on the img el in the modal (html); js reads class as an array
 
 const imageBasePath = "buildingImg/WebProjectLowRes";
 
 const projects = {
     carollCreek: {
         images: [
-            `${imageBasePath}/CarollCreek4.jpg`,
-            `${imageBasePath}/CarollCreek5.jpg`,
-            `${imageBasePath}/CarollCreek6.jpg`,
-            `${imageBasePath}/CarollCreekHistoricDistrict1.jpg`,
-            `${imageBasePath}/CarollCreekHistoricDistrict2.jpg`,
-            `${imageBasePath}/CarollCreekHistoricDistrict3.jpg`,
+            // img count: 7
+            `${imageBasePath}/carollCreek1.jpg`,
+            `${imageBasePath}/carollCreek2.jpg`,
+            `${imageBasePath}/carollCreek3.jpg`,
+            `${imageBasePath}/carollCreek4.jpg`,
+            `${imageBasePath}/carollCreek5.jpg`,
+            `${imageBasePath}/carollCreek6.jpg`,
+            `${imageBasePath}/carollCreek7.jpg`
         ]
     },
     headquartersVA: {
         images: [
-            `${imageBasePath}/CommericalHq3.jpg`,
-            `${imageBasePath}/CommericalHqVa.jpg`,
+            // img count: 5
+            `${imageBasePath}/hqVA1.jpg`,
+            `${imageBasePath}/hqVA2.jpg`,
+            `${imageBasePath}/hqVA3.jpg`,
+            `${imageBasePath}/hqVA4.jpg`,
+            `${imageBasePath}/hqVA5.jpg`
         ]
     },
     masterplanGreenbelt: {
         images: [
-            `${imageBasePath}/MasterplanGreenbeltMd1.jpg`,
-            `${imageBasePath}/img6.jpg`
+            // img count: 2
+            `${imageBasePath}/masterplanGreenbelt1.jpg`,
+            `${imageBasePath}/masterplanGreenbelt2.jpg`
+            
+        ]
+    },
+    bethesdaAve: {
+        images: [
+            //img count: 2
+            `${imageBasePath}/bethesdaAve1.jpg`,
+            `${imageBasePath}/bethesdaAve2.jpg`
+        ]
+    },
+    auburnAve: {
+        images: [
+            // img count: 2
+            `${imageBasePath}/auburnAve1.jpg`,
+            `${imageBasePath}/auburnAve2.jpg`
         ]
     }
 }
-
-
 
 // When the user clicks the button, open the modal 
 /*
@@ -56,11 +76,13 @@ function openModal(project) {
     for (let i = 0; i < carouselImages.length; i++) {
         const carouselImageEl = carouselImages[i];
         // console.log({ project })
-        console.log( "project images:", projects[project].images[i])
+        // console.log( "project images:", projects[project].images[i])
 
         const projectImage = projects[project].images[i];
+        //array within an array
 
-        carouselImageEl.src = projectImage || `${imageBasePath}/WebProjectLowResNames.jpg`;
+        carouselImageEl.src = projectImage || projects[project].images[0];
+        // carouselImageEl.src = projectImage || `${imageBasePath}/WebProjectLowResNames.jpg`;
         modal.style.display = "block"
     }
 }
